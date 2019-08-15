347eaed40b20f3049085ed346e92324a58fc1fdd
violance228 <sviripa1234@gmail.com>
2019-08-15 21:53:32 +0300

    Initial commit

---
+++ src/main/java/com/violence/VideoConverter.java
@@ -0,0 +1,41 @@
+package com.violence;
+
+import ws.schild.jave.AudioAttributes;
+import ws.schild.jave.Encoder;
+import ws.schild.jave.EncodingAttributes;
+import ws.schild.jave.MultimediaObject;
+
+import java.io.File;
+
+public class VideoConverter {
+
+    public void convertVideoToAudio(File video){
+        File audio = new File("G:\\project\\videoToAudioConverter\\audioM.mp3");
+        AudioAttributes audioAttributes = new AudioAttributes();
+        audioAttributes.setBitRate(128000);
+        audioAttributes.setChannels(2);
+        audioAttributes.setSamplingRate(44100);
+
+        EncodingAttributes attributes = new EncodingAttributes();
+        attributes.setFormat("mp4");
+        attributes.setAudioAttributes(audioAttributes);
+
+        Encoder encoder = new Encoder();
+        try {
+            encoder.encode(new MultimediaObject(video), audio, attributes);
+        }catch (Exception e) {
+            System.out.println("can't encode");
+        }
+    }
+
+    public static void main(String [] args){
+        VideoConverter vta = new VideoConverter();
+        try{
+            vta.convertVideoToAudio(new File("G:\\project\\videoToAudioConverter\\videoToAudi.mp4"));
+        }
+        catch(Exception e){
+            System.out.println("Could not open video file");
+        }
+    }
+
+}
